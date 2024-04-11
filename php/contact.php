754577898057fe32b11205515894597371d80b53
jdewb <dewberry.john@gmail.com>
2024-04-11 09:40:30 -0500

    Test

---
+++ php/contact.php
@@ -0,0 +1,38 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$message = $_POST["msg"];
+ 
+$EmailTo = "hello@jdewb.com";
+$Subject = "JDEWB MESSAGE:";
+ 
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+ 
+$Body .= "Message: ";
+$Body .= "\n";
+$Body .= $message;
+$Body .= "\n";
+ 
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+ 
+// redirect to success page
+if ($success){
+   echo "OK";
+}else{
+    echo "Something went wrong.";
+}
+ 
+?>
